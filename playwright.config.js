9f83593f5311467c451313efc960c66a1e1423ef
Kashyap Vishwakarma <kashyap.vishwakarma@ignatiuz.com>
2025-11-04 11:17:15 +0530

    newallure

--- playwright.config.js
+++ playwright.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [
+  reporter: (process.env.CI || process.env.ALLURE) ? [
     ["html"],
     ["allure-playwright"]
   ] : "html",
